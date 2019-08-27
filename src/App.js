c62c4da4c68d21ee9915f7b802e9f3d85506dc96
laughing cat <lc.laughing.cat@gmail.com>
2019-08-27 22:23:00 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Ranking from './containers/Rranking';
+import Nav from './containers/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path="/all" component={Ranking} />
+          <Route
+            path="/category/1"
+            render={() => <Redirect to="/all" />}
+          />
+          <Route
+            path="/category/:id"
+            render={
+              ({match}) => <Ranking categoryId={match.params.id} />
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
+
 
 export default App;
